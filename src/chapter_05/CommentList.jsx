c6dfdc5132688cc16d7e7b546f9e85f23c9507fa
starlash7 <yang92sw@gmail.com>
2024-05-22 01:37:03 +0900

    props

---
+++ src/chapter_05/CommentList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Comment from "./Comment";
+
+function CommentList() {
+  return (
+    <div>
+      <Comment />
+    </div>
+  );
+}
+
+export default CommentList;
--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import Clock from "./chapter_04/clock";
+import CommentList from "./chapter_05/CommentList";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-setInterval(() => {
-  root.render(
-    <React.StrictMode>
-      <Clock />
-    </React.StrictMode>
-  );
-});
+ReactDOM.render(
+  <React.StrictMode>
+    <CommentList />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
